aef7694410d983a0f3d717ec0f47186cd937b117
ghlim00 <p1ayer.one.potter@gmail.com>
2026-02-08 16:14:30 +0900

    feat: 로그인 redirect 변경 반영

--- Frontend/src/router/index.jsx
+++ Frontend/src/router/index.jsx
@@ -5,6 +5,7 @@ import CommunityEditPage from "@pages/Community/CommunityEditPage";
 import CommunityDetailPage from "@pages/Community/CommunityDetailPage";
 import ProblemListPage from "@pages/Problems/ProblemListPage";
 import MyPage from "@pages/MyPage/MyPageGate";
+import AuthCallbackPage from "@pages/AuthCallbackPage";
 
 const routerInfo = [
   {
@@ -14,6 +15,9 @@ const routerInfo = [
       {
         index: true,
         element: <div className="p-10 text-2xl">홈 화면입니다!</div>,
+      },
+      { path: "auth/callback", 
+        element: <AuthCallbackPage /> 
       },
       {
         path: "community",
